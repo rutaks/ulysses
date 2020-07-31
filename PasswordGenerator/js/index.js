4c8386a11c7aa6d81a677ba95451557392f438ec
rutaks <rootsum.dev@gmail.com>
2020-07-31 06:58:04 +0200

    Add copy to clipboard process

--- PasswordGenerator/js/index.js
+++ PasswordGenerator/js/index.js
@@ -1,7 +1,29 @@
 const btn = document.getElementById("btn");
-btn.onclick = () => generatePassword();
+const copyBtn = document.getElementById("copy-btn");
+const alertPane = document.querySelector(".alert-pane");
+const passwordOutput = document.getElementById("password-output");
 
-function generatePassword() {
+//================
+// EVENT LISTENERS
+//================
+
+btn.onclick = () => generatePassword();
+copyBtn.onclick = () => copyToClipboard();
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    alertPane.classList.remove("active");
+  }
+});
+
+//================
+// FUNCTIONS
+//================
+
+/**
+ * Function to randomly generate a password.
+ * Attaches the password to output text
+ */
+const generatePassword = () => {
   const charsToUse =
     "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*(){};<>?_+-=";
   const passwordLen = 16;
@@ -10,5 +32,20 @@ function generatePassword() {
     let randomNum = Math.floor(Math.random() * charsToUse.length);
     password += charsToUse.substring(randomNum, randomNum + 1);
   }
-  document.getElementById("password-output").value = password;
-}
+  passwordOutput.value = password;
+};
+
+/**
+ * Function to copy value in password output box to clipboard.
+ * Gives you a prompt showing that the password was copied.
+ * Does not copy when no password is generated
+ */
+const copyToClipboard = () => {
+  if (passwordOutput.value.length > 0) {
+    passwordOutput.select();
+    passwordOutput.setSelectionRange(0, 9999);
+    document.execCommand("copy");
+    alertPane.classList.toggle("active");
+    alertPane.innerHTML = `Password generated <br/> ${passwordOutput.value} <br/>Press esc to remove prompt`;
+  }
+};
